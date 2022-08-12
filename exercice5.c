b6941e322afe6b8dd92936aeeed3fc4f7f9b9397
PHAM Hugo <hakubest13@gmail.com>
2022-08-12 14:41:25 +0200

    message

---
+++ exercice5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void sum_array(int t1[], int t2[], int t[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        t[i] = t1[i] + t2[i];
+    }
+}
+
+//Je ne sais pas commment retourner une liste, alors je fais en sorte
+//qu'elle soit une entrée et qu'elle soit modifiée durant la fonction
+
+int main()
+{
+    return 0;
+}
